75dece5a58c4ecb0647479df9a640961ae591a6c
andrey <avolkov123@mail.ru>
2019-03-06 19:02:00 +0300

    циклы испытания

---
+++ 3. Циклы/5. Произведение чётных.js
@@ -0,0 +1,19 @@
+var lastNumber = 5;
+var multiplicationResult = 1;
+
+for(var i = 1; i <= lastNumber; i++){
+   if(i % 2 === 0){
+     multiplicationResult *= i;
+     console.log(i);
+   }
+}
+console.log('Результат равен ' + multiplicationResult);
+/* Техническое задание
+
+Напишите универсальную программу, которая находит произведение всех чётных чисел из последовательности от 1 до n.
+
+Число, до которого идёт последовательность (включительно), записано в переменную lastNumber
+
+Найдите произведение всех чисел и сохраните результат в переменную multiplicationResult.
+
+*/
