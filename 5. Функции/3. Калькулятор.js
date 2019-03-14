94a7d3d50340d7941a5cb670b9b8fa89696dc3fa
andryshan <avolkov123@mail.ru>
2019-03-14 19:03:33 +0300

    функции-испытания

---
+++ 5. Функции/3. Калькулятор.js
@@ -0,0 +1,34 @@
+var calculate = function(firstNumber, secondNumber, operator){
+  var result = 0;
+  switch(operator){
+    case '+':
+      result = parseInt(firstNumber, 10) + parseInt(secondNumber, 10);
+      break;
+    case '-':
+      result = parseInt(firstNumber, 10) - parseInt(secondNumber, 10);
+      break;
+    case '/':
+      result = parseInt(firstNumber, 10) / parseInt(secondNumber, 10);
+      break;
+    case '*':
+      result = parseInt(firstNumber, 10) * parseInt(secondNumber, 10);
+      break;
+  }
+  return result;
+}
+
+/* Техническое задание
+
+Мяу! Напиши функцию calculate, которая будет распознавать пользовательский ввод, совершать математические операции и возвращать результат.
+
+Параметры функции — firstNumber, secondNumber и operator.
+
+Параметры firstNumber, secondNumber — операнды, параметр operator - оператор.
+
+Все значения приходят в виде строк, в том числе и операторы. В третьем параметре содержится один из операторов: сложение ('+'), вычитание ('-'), умножение ('*'), деление ('/').
+
+В зависимости от оператора программа должна совершать разные операции: складывать первое число со вторым, вычитать из первого числа второе, умножать первое на второе, делить первое на второе.
+
+Не забудь сделать из строк с числами настоящие числа.
+
+*/
